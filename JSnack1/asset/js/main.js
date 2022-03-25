414ed3df5d3b24fa32aa3b56a2c4a24e0938f1d7
simoneluca00 <si.zaino@libero.it>
2022-03-25 15:57:19 +0100

    JSnack 1 completed

---
+++ JSnack1/asset/js/main.js
@@ -0,0 +1,57 @@
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+
+const bicycles = [
+    {
+        'nome': 'Argon',
+        'pesoKg': 7
+    },
+
+    {
+        'nome': 'Lapierre',
+        'pesoKg': 6.5
+    },
+
+    {
+        'nome': 'Kuota',
+        'pesoKg': 8.5
+    },
+
+    {
+        'nome': 'Stelbel',
+        'pesoKg': 9
+    },
+
+    {
+        'nome': 'Olympia',
+        'pesoKg': 6
+    }
+]
+
+let pesoMinore = bicycles[0];
+
+for (let i = 0; i < bicycles.length; i++) {
+    
+    // singola bicicletta (oggetto) 
+    let singleBike = bicycles[i];
+
+    if (singleBike.pesoKg < pesoMinore.pesoKg) {
+        pesoMinore = singleBike;
+    }
+}
+
+// destructuring delle chiavi degli oggetti appartenenti all'array "bicycles"
+let {nome,pesoKg} = bicycles;
+
+// stampa della bici che ha il peso minore
+document.getElementById('container').innerHTML = 
+`
+    La bici da corsa 
+    <span style="font-weight:bold;">
+        ${pesoMinore.nome}
+    </span> 
+    è quella che ha un peso minore: 
+    <span style="font-weight:bold;">
+        ${pesoMinore.pesoKg}
+    </span> kg.
+`

623d5e435df47ef9bb239ba1952675d3ad7a0f62
simoneluca00 <si.zaino@libero.it>
2022-03-25 16:29:55 +0100

    JSnack2 completed

---
+++ JSnack2/asset/js/main.js
@@ -0,0 +1,80 @@
+// Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+// Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti.
+// Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console
+
+const footballTeams = [
+    {
+        'nome': 'Inter',
+        'puntiFatti': 0,
+        'falliSubiti': 0
+    },
+
+    {
+        'nome': 'Milan',
+        'puntiFatti': 0,
+        'falliSubiti': 0
+    },
+
+    {
+        'nome': 'Juventus',
+        'puntiFatti': 0,
+        'falliSubiti': 0
+    },
+
+    {
+        'nome': 'Napoli',
+        'puntiFatti': 0,
+        'falliSubiti': 0
+    },
+
+    {
+        'nome': 'Atalanta',
+        'puntiFatti': 0,
+        'falliSubiti': 0
+    },
+
+    {
+        'nome': 'Fiorentina',
+        'puntiFatti': 0,
+        'falliSubiti': 0
+    },
+
+    {
+        'nome': 'Roma',
+        'puntiFatti': 0,
+        'falliSubiti': 0
+    }
+]
+
+for (let i = 0; i < footballTeams.length; i++) {
+    
+    let numeroRandomFalli = getRandomInt(2,60);
+    let numeroRandomPunti = getRandomInt(2,100)
+    
+    let team = footballTeams[i];
+
+    team.falliSubiti = numeroRandomFalli;
+    team.puntiFatti = numeroRandomPunti;
+
+    let {nome, falliSubiti} = team;
+    
+    document.getElementById('container').innerHTML += 
+    `
+        <li>
+            La squadra
+            <span style="font-weight: bold;">${nome}</span> 
+            ha subito 
+            <span style="font-weight: bold;">${falliSubiti}</span> 
+            falli.
+        </li>
+    `
+
+    console.log(`La squadra ${nome} ha subito ${falliSubiti} falli`)
+}
+
+
+// funzione per generare numeri random
+function getRandomInt(min, max) {
+    return Math.floor((Math.random() * max)+ min);
+}
